91edfe83337767651cb1231552b288dfa809b4ca
TheMarceloK <115849519+TheMarceloK@users.noreply.github.com>
2023-03-30 00:02:38 -0300

    Agachamento e corrida
    alterei o cod do player para acrescentar essa mecânica

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,12 @@ public class Player : MonoBehaviour
     private Rigidbody rigidbody;
     private Vector3 movement;
     [SerializeField]
-    private float speed = 5f;
+    private float walkspeed, runningspeed,crouchspeed, crouchYScale;
+    private float startYScale;
+    private float speed;
+
+    public KeyCode runningkey = KeyCode.LeftShift;
+    public KeyCode crouchkey = KeyCode.LeftControl;
 
     public int Bullets = 6;
 
@@ -19,6 +24,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        startYScale= transform.localScale.y;
     }
 
     private void Update()
@@ -36,15 +42,54 @@ public class Player : MonoBehaviour
                 Bullets--;
             }
         }
+       
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             Bullets = 6;
         }
+        if (Input.GetKeyDown(crouchkey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rigidbody.AddForce(Vector3.down*5f,ForceMode.Impulse);
+        }
+        if (Input.GetKeyUp(crouchkey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+            rigidbody.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        StateHandler();
     }
-
+    
     private void FixedUpdate()
     {
         rigidbody.MovePosition(rigidbody.position + (movement * speed * Time.fixedDeltaTime));
     }
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        running,
+        crounching
+    }
+    private void StateHandler()
+    {
+        if (Input.GetKey(runningkey))
+        {
+            state = MovementState.running;
+            speed = runningspeed;
+        }
+        else { state = MovementState.walking;
+            speed = walkspeed;
+        }
+        
+        if (Input.GetKey(crouchkey))
+        {
+            state = MovementState.crounching;
+            speed = crouchspeed;
+        }
+    }
 }
+/*control agacha e faz se mover mais devagar
+ shift faz correr
+ */
